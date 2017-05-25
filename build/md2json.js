22591456e8c8d20ac9355e3c542a307c097c5071
Dave Machado <dave@davemachado.com>
2017-05-25 11:37:33 -0400

    Trim Markdown Go! out of Link string

--- build/md2json.js
+++ build/md2json.js
@@ -1,7 +1,11 @@
 fs = require('fs')
 
 function md_trim(str) {
-    return str.replace(/(^\s+)|(\s+$)/g, "");
+    str = str.replace(/(^\s+)|(\s+$)/g, "");
+    if (str.lastIndexOf("[Go!]", 0) === 0) {
+        str = str.replace("[Go!]", "").slice(1, -1);
+    }
+    return str;
 }
 
 function handle(filename, anchor) {
