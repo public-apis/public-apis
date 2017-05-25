b0ef43db583b02ac7d519d4a8a346ce17b45077c
Dave Machado <dave@davemachado.com>
2017-05-25 00:32:50 -0400

    Use Python tool to beautify JSON into unminified /json file

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 mkdir -p ../json
-node md2json.js ../README.md > ../json/api.json
+node md2json.js ../README.md > ../json/api.min.json
+python -m json.tool ../json/api.min.json > ../json/api.json
 cat ../json/api.json
-ls -l ..
-ls -l ../json
