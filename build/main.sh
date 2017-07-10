ab045f33f391f6a16363856f96cd1ab3ddaf5d0b
Dave Machado <dave@davemachado.com>
2017-07-10 01:41:50 -0400

    Update main script and Travis config

--- build/main.sh
+++ build/main.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" == "master" ]
-then
-    awesome_bot README.md --allow-ssl --allow 403,302
+echo "running format validation..."
+./validate.rb ../README.md
+
+if ["$?" == 0]; then
+    echo "format validation PASSED"
+else
+    echo "format validation FAILED"
 fi
 
-./validate.rb ../README.md
+if [ "$TRAVIS_BRANCH" == "master" ]
+then
+    echo "running link validation..."
+    awesome_bot README.md --allow-ssl --allow 403,302
+fi
