fbd07b8f4bf9848a45b189d226d8ead6fdc4c4ff
Dave Machado <dave@davemachado.com>
2017-07-28 11:40:21 -0400

    Add padding check for each line value

--- build/validate_format.rb
+++ build/validate_format.rb
@@ -18,35 +18,61 @@ File.foreach(filename).with_index do | line, line_num |
         if line.eql? "|---|---|---|---|---|\n"
             next
         end
+
         values = line.split("|")
+
+        values.each.with_index do |val, val_index|
+            msg = ""
+            case val_index
+            when 1..5
+                if val[0] != " " || val[val.length-1] != " "
+                    case val_index
+                    when 1
+                        msg = "spacing on Title is invalid"
+                    when 2
+                        msg = "spacing on Description is invalid"
+                    when 3
+                        msg = "spacing on Auth is invalid"
+                    when 4
+                        msg = "spacing on HTTPS is invalid"
+                    when 5
+                        msg = "spacing on Link is invalid"
+                    end
+                end
+            end
+            if msg != ""
+                add_error(line_num, "#{msg} (pad before and after string)")
+            end
+        end
+
         ################# DESCRIPTION ################
         # First character should be capitalized
         desc_val = values[2].lstrip.chop
         if !/[[:upper:]]/.match(desc_val[0])
-            add_error(line_num, "Invalid Description (first char not uppercase): #{desc_val}")
+            add_error(line_num, "invalid Description (first char not uppercase): #{desc_val}")
         end
         # value should not be punctuated
         last_char = desc_val[desc_val.length-1]
         if punctuation.include?(last_char)
-            add_error(line_num, "Invalid Description (description should not end with \"#{last_char}\")")
+            add_error(line_num, "invalid Description (description should not end with \"#{last_char}\")")
         end
         #################### AUTH ####################
         # Values should conform to valid options only
         auth_val = values[3].lstrip.chop.tr('``', '')
         if !auth_keys.include?(auth_val)
-            add_error(line_num, "Invalid Auth (not a valid option): #{auth_val}")
+            add_error(line_num, "invalid Auth (not a valid option): #{auth_val}")
         end
         #################### HTTPS ###################
         # Values should be either "Yes" or "No"
         https_val = values[4].lstrip.chop
         if !https_keys.include?(https_val)
-            add_error(line_num, "(#{line_num}) Invalid HTTPS: (must use \"Yes\" or \"No\"): #{https_val}")
+            add_error(line_num, "invalid HTTPS (must use \"Yes\" or \"No\"): #{https_val}")
         end
         #################### LINK ####################
         # Url should be wrapped in "[Go!]" view
         link_val = values[5].lstrip.chop
         if !link_val.start_with?("[Go!](") || !link_val.end_with?(')')
-            add_error(line_num, "(#{line_num}) Invalid Link: (format should be \"[Go!](<LINK>)\"): #{link_val}")
+            add_error(line_num, "invalid Link (format should be \"[Go!](<LINK>)\"): #{link_val}")
         end
     end
 end
