105c745ac93e8ef808050a09c8dc69a0e705de10
Dave Machado <dave@davemachado.com>
2017-07-28 11:49:32 -0400

    move segment check logic to method

--- build/validate_format.rb
+++ build/validate_format.rb
@@ -7,8 +7,21 @@ args = ARGV
 filename = args[0]
 $errors = []
 
-def add_error(line_num, message)
-    $errors.push("(L%03d) #{message}" % line_num)
+def add_error(line_num, val_index, message)
+    case val_index
+    when 1
+        segment = "Title"
+    when 2
+        segment = "Description"
+    when 3
+        segment = "Auth"
+    when 4
+        segment = "HTTPS"
+    when 5
+        segment = "Link"
+    end
+
+    $errors.push("(L%03d) (#{segment}) #{message}" % line_num)
 end
 
 File.foreach(filename).with_index do | line, line_num |
@@ -26,22 +39,8 @@ File.foreach(filename).with_index do | line, line_num |
             case val_index
             when 1..5
                 if val[0] != " " || val[val.length-1] != " "
-                    case val_index
-                    when 1
-                        msg = "spacing on Title is invalid"
-                    when 2
-                        msg = "spacing on Description is invalid"
-                    when 3
-                        msg = "spacing on Auth is invalid"
-                    when 4
-                        msg = "spacing on HTTPS is invalid"
-                    when 5
-                        msg = "spacing on Link is invalid"
-                    end
+                add_error(line_num, val_index, "spacing is invalid (pad before and after string)")
                 end
-            end
-            if msg != ""
-                add_error(line_num, "#{msg} (pad before and after string)")
             end
         end
 
@@ -49,30 +48,30 @@ File.foreach(filename).with_index do | line, line_num |
         # First character should be capitalized
         desc_val = values[2].lstrip.chop
         if !/[[:upper:]]/.match(desc_val[0])
-            add_error(line_num, "invalid Description (first char not uppercase): #{desc_val}")
+            add_error(line_num, 2, "first char not uppercase")
         end
         # value should not be punctuated
         last_char = desc_val[desc_val.length-1]
         if punctuation.include?(last_char)
-            add_error(line_num, "invalid Description (description should not end with \"#{last_char}\")")
+            add_error(line_num, 2, "description should not end with \"#{last_char}\"")
         end
         #################### AUTH ####################
         # Values should conform to valid options only
         auth_val = values[3].lstrip.chop.tr('``', '')
         if !auth_keys.include?(auth_val)
-            add_error(line_num, "invalid Auth (not a valid option): #{auth_val}")
+            add_error(line_num, 3, "not a valid option: #{auth_val}")
         end
         #################### HTTPS ###################
         # Values should be either "Yes" or "No"
         https_val = values[4].lstrip.chop
         if !https_keys.include?(https_val)
-            add_error(line_num, "invalid HTTPS (must use \"Yes\" or \"No\"): #{https_val}")
+            add_error(line_num, 4, "must use \"Yes\" or \"No\": #{https_val}")
         end
         #################### LINK ####################
         # Url should be wrapped in "[Go!]" view
         link_val = values[5].lstrip.chop
         if !link_val.start_with?("[Go!](") || !link_val.end_with?(')')
-            add_error(line_num, "invalid Link (format should be \"[Go!](<LINK>)\"): #{link_val}")
+            add_error(line_num, 5, "format should be \"[Go!](<LINK>)\": #{link_val}")
         end
     end
 end
