ffddb7bbae8c840c54d0a4510905588ab36c7252
Dave Machado <dave@davemachado.com>
2017-07-28 11:54:43 -0400

    use constants to represent line segment index

--- build/validate_format.rb
+++ build/validate_format.rb
@@ -3,21 +3,26 @@
 auth_keys = ['apiKey', 'OAuth', 'X-Mashape-Key', 'No']
 punctuation = ['.', '?', '!']
 https_keys = ['Yes', 'No']
-args = ARGV
-filename = args[0]
+
+INDEX_TITLE = 1
+INDEX_DESCRIPTION = 2
+INDEX_AUTH = 3
+INDEX_HTTPS = 4
+INDEX_LINK = 5
+filename = ARGV[0]
 $errors = []
 
 def add_error(line_num, val_index, message)
     case val_index
-    when 1
+    when INDEX_TITLE
         segment = "Title"
-    when 2
+    when INDEX_DESCRIPTION
         segment = "Description"
-    when 3
+    when INDEX_AUTH
         segment = "Auth"
-    when 4
+    when INDEX_HTTPS
         segment = "HTTPS"
-    when 5
+    when INDEX_LINK
         segment = "Link"
     end
 
@@ -37,7 +42,7 @@ File.foreach(filename).with_index do | line, line_num |
         values.each.with_index do |val, val_index|
             msg = ""
             case val_index
-            when 1..5
+            when INDEX_TITLE..INDEX_LINK
                 if val[0] != " " || val[val.length-1] != " "
                 add_error(line_num, val_index, "spacing is invalid (pad before and after string)")
                 end
@@ -46,32 +51,32 @@ File.foreach(filename).with_index do | line, line_num |
 
         ################# DESCRIPTION ################
         # First character should be capitalized
-        desc_val = values[2].lstrip.chop
+        desc_val = values[INDEX_DESCRIPTION].lstrip.chop
         if !/[[:upper:]]/.match(desc_val[0])
-            add_error(line_num, 2, "first char not uppercase")
+            add_error(line_num, INDEX_DESCRIPTION, "first char not uppercase")
         end
         # value should not be punctuated
         last_char = desc_val[desc_val.length-1]
         if punctuation.include?(last_char)
-            add_error(line_num, 2, "description should not end with \"#{last_char}\"")
+            add_error(line_num, INDEX_DESCRIPTION, "description should not end with \"#{last_char}\"")
         end
         #################### AUTH ####################
         # Values should conform to valid options only
-        auth_val = values[3].lstrip.chop.tr('``', '')
+        auth_val = values[INDEX_AUTH].lstrip.chop.tr('``', '')
         if !auth_keys.include?(auth_val)
-            add_error(line_num, 3, "not a valid option: #{auth_val}")
+            add_error(line_num, INDEX_AUTH, "not a valid option: #{auth_val}")
         end
         #################### HTTPS ###################
         # Values should be either "Yes" or "No"
-        https_val = values[4].lstrip.chop
+        https_val = values[INDEX_HTTPS].lstrip.chop
         if !https_keys.include?(https_val)
-            add_error(line_num, 4, "must use \"Yes\" or \"No\": #{https_val}")
+            add_error(line_num, INDEX_HTTPS, "must use \"Yes\" or \"No\": #{https_val}")
         end
         #################### LINK ####################
         # Url should be wrapped in "[Go!]" view
-        link_val = values[5].lstrip.chop
+        link_val = values[INDEX_LINK].lstrip.chop
         if !link_val.start_with?("[Go!](") || !link_val.end_with?(')')
-            add_error(line_num, 5, "format should be \"[Go!](<LINK>)\": #{link_val}")
+            add_error(line_num, INDEX_LINK, "format should be \"[Go!](<LINK>)\": #{link_val}")
         end
     end
 end
