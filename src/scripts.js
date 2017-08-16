43780c40b5d428295657c41439f858cf77128016
davemachado <dave@davemachado.com>
2017-08-15 23:00:45 -0400

    add basic table styling

--- src/scripts.js
+++ src/scripts.js
@@ -42,3 +42,24 @@ new Vue({
 		}
 	}
 }).$mount('#app');
+
+function filterRows() {
+  // Declare variables 
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("searchbox");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
