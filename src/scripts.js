6b03ab1af6df8eeaeaa00a2ccdde61f26992cc68
davemachado <dave@davemachado.com>
2017-08-17 12:20:52 -0400

    Use Vue method to filter search listings

--- src/scripts.js
+++ src/scripts.js
@@ -34,23 +34,3 @@ new Vue({
 		}
 	}
 }).$mount('#app');
-
-function filterRows() {
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("searchbox");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("entries");
-  tr = table.getElementsByTagName("tr");
-
-  // Loop through all table rows and hide those who don't match the search
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    } 
-  }
-}
