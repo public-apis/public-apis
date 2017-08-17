d148df03db2b74eee905f3a4f98586428c0a0cc2
davemachado <dave@davemachado.com>
2017-08-16 23:10:32 -0400

    remove extra strings

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,11 @@
  function loadJSON(callback) {   
     var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'https://raw.githubusercontent.com/toddmotto/public-apis/master/json/entries.min.json', true); // Replace 'my_data' with the path to your file
+    xobj.open('GET', 'https://raw.githubusercontent.com/toddmotto/public-apis/master/json/entries.min.json', true);
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            // Required use of an anonymous callback as .open will NOT return a value
+            // but simply returns undefined in asynchronous mode
             callback(xobj.responseText);
           }
     };
