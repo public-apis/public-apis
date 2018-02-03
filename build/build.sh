b0409e8ae2e89d99af16ee6701309767a47aac0a
davemachado <dave@davemachado.com>
2018-02-03 13:30:53 -0500

    Migrate JSON logic to api project

--- build/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# create json directory if not already present
-mkdir -p ../json
-# parse API README and print (minified) JSON to stdout, redirect to /json
-./md2json.py ../README.md > ../json/entries.min.json
-# beautify the previously created JSON file, redirect to /json
-python -m json.tool ../json/entries.min.json > ../json/entries.json
