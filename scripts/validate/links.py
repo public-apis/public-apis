50ad807e5c9ef953ea88b496f67e8fc4aff249ca
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2022-01-13 16:23:01 -0300

    Show number of links with a possible error

--- scripts/validate/links.py
+++ scripts/validate/links.py
@@ -225,6 +225,11 @@ if __name__ == '__main__':
 
     errors = check_if_list_of_links_are_working(links)
     if errors:
+
+        num_errors = len(errors)
+        print(f'Apparently {num_errors} links are not working properly. See in:')
+
         for error_message in errors:
             print(error_message)
+
         sys.exit(1)
