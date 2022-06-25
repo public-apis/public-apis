738820a861637a7a5d4d7d30991525d886f21e5d
yonatan <68735811+svg-mn@users.noreply.github.com>
2022-06-25 22:06:09 +0300

    Update links.py
    in line 15 the value of the variable was too long so you would have to scroll aside, I lowered half of it.
    hope its ok :)

--- scripts/validate/links.py
+++ scripts/validate/links.py
@@ -12,8 +12,8 @@ from requests.models import Response
 def find_links_in_text(text: str) -> List[str]:
     """Find links in a text and return a list of URLs."""
 
-    link_pattern = re.compile(r'((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'\".,<>?«»“”‘’]))')
-
+    link_pattern = re.compile(r'((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\(['
+                              r'^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'\".,<>?«»“”‘’]))')
     raw_links = re.findall(link_pattern, text)
 
     links = [
