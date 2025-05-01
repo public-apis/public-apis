cd66c7004382f0cc46f20669708a61b43ee00357
Arash Khajavi <arash.khajavi.ghk123@gmail.com>
2025-05-01 21:55:20 +0300

    Update format.py

--- scripts/validate/format.py
+++ scripts/validate/format.py
@@ -37,7 +37,7 @@ CategoriesLineNumber = Dict[str, int]
 def error_message(line_number: int, message: str) -> str:
     line = line_number + 1
     return f'(L{line:03d}) {message}'
-def get_categories_content(contents: List[str]) -> Tuple[categories, category_line_num]:
+def get_categories_content(contents: List[str]) -> Tuple[Categories, categoryLineNumber]:
    categories = {}
    category_line_num = {}
    for line_num, line_content in enumerate(contents):
