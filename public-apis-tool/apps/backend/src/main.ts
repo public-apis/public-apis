6ab7404fc9e71f1730fa39be3a88494a9daa353e
L3gvccy <oleksandr_iv@knu.ua>
2026-02-07 13:38:59 +0200

    configure dotenv

--- public-apis-tool/apps/backend/src/main.ts
+++ public-apis-tool/apps/backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import "dotenv/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
